7cb11d296b23cecada24a4b07822986bda22979c
Olaegbe Samuel <25031528+goodhands@users.noreply.github.com>
2020-01-03 18:42:56 +0100

    Types in Python

---
+++ intro/Types.py
@@ -0,0 +1,13 @@
+#   None Type
+#The NoneType has only 1 single possible value: None
+type(None)  #NoneType
+
+#None is the default return value of a function
+return_value = print('Hello world')
+print(return_value)  #None
+
+#   Boolean Type
+#The Bool has only 2 possible values: True && False and they are case sensitives.
+result = (4 < 5)
+result  #True
+type(result)    #bool
